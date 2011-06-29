38241186c14eccaeab165e8ac1b77b952de814b9
jthompson <jessica.thompson@mail.mcgill.ca>
2011-06-29 16:20:25 -0400

    bounding box bug fixes

--- MEI2couchdb.py
+++ MEI2couchdb.py
@@ -38,24 +38,35 @@ def getLocation(seq, meifile, zones):
         If the sequence is contained in a single system, only one location will be stored. If the sequence
         spans two systems, a list of two locations will be stored.
     """
+    ulys = []
+    lrys = []
+    twosystems=0
+    endofsystem = len(seq)-1
     if meifile.get_system(seq[0]) != meifile.get_system(seq[len(seq)-1]): #then the sequence spans two systems and we must store two seperate locations to highlight
+        twosystems=1
         for i in range(1,len(seq)):
             # find the last note on the first system and the first note on the second system
             if meifile.get_system(seq[i-1]) != meifile.get_system(seq[i]): 
+                endofsystem = i # this will be the index of the first note on second system
                 ulx1 =  int(findbyID(zones, seq[0].parent.parent.facs).ulx)
-                uly1 =  int(findbyID(zones, seq[0].parent.parent.facs).uly)
                 lrx1 =  int(findbyID(zones, seq[i-1].parent.parent.facs).lrx)
-                lry1 =  int(findbyID(zones, seq[i-1].parent.parent.facs).lry)
                 ulx2 =  int(findbyID(zones, seq[i].parent.parent.facs).ulx)
-                uly2 =  int(findbyID(zones, seq[i].parent.parent.facs).uly)
                 lrx2 =  int(findbyID(zones, seq[-1].parent.parent.facs).lrx)
-                lry2 =  int(findbyID(zones, seq[-1].parent.parent.facs).lry)
-                return [{"ulx": int(ulx1) ,"uly": int(uly1), "height": abs(uly1 - lry1), "width": abs(ulx1 - lrx1)},{"ulx": int(ulx2) ,"uly": int(uly2), "height": abs(uly2 - lry2), "width": abs(ulx2 - lrx2)}] 
     else: # the sequence is contained in one system and only one box needs to be highlighted
         ulx =  int(findbyID(zones, seq[0].parent.parent.facs).ulx)
-        uly =  int(findbyID(zones, seq[0].parent.parent.facs).uly)
         lrx =  int(findbyID(zones, seq[-1].parent.parent.facs).lrx)
-        lry =  int(findbyID(zones, seq[-1].parent.parent.facs).lry)
+    for note in seq:
+        ulys = ulys + [int(findbyID(zones, note.parent.parent.facs).uly)]
+        lrys = lrys + [int(findbyID(zones, note.parent.parent.facs).lry)]
+    if twosystems:
+        uly1 = min(ulys[:endofsystem])
+        uly2 = min(ulys[endofsystem:])
+        lry1 = max(lrys[:endofsystem])
+        lry2 = max(lrys[endofsystem:])
+        return [{"ulx": int(ulx1) ,"uly": int(uly1), "height": abs(uly1 - lry1), "width": abs(ulx1 - lrx1)},{"ulx": int(ulx2) ,"uly": int(uly2), "height": abs(uly2 - lry2), "width": abs(ulx2 - lrx2)}]      
+    else:
+        uly =  min(ulys)
+        lry = max(lrys)
         return [{"ulx": int(ulx) ,"uly": int(uly), "height": abs(uly - lry), "width": abs(ulx - lrx)}]
 
 def getNeumes(seq):
@@ -125,7 +136,7 @@ def storeText(lines, zones, textdb):
         facs = str(line.attribute_by_name('facs').value)
         zone = findbyID(zones, facs)
         ulx = int(zone.ulx)
-        uly = int(zone.ulx)
+        uly = int(zone.uly)
         lrx = int(zone.lrx)
         lry = int(zone.lry)
         textdb.save({'pagen': pagen, 'text': text, 'location': {"ulx": ulx ,"uly": uly, "height": abs(uly - lry), "width": abs(ulx - lrx)}})
@@ -138,7 +149,7 @@ meifiles = []
 for bd, dn, fn in os.walk(path):
     for f in fn:
         if not (('uncorr' in f) and os.path.exists(os.path.join(bd,f[0:5]+'corr.mei'))): # if current is uncorr version and corr version exists, don't add to list
-            meifiles = meifiles + [os.path.join(bd,f)]
+            meifiles = meifiles + [os.path.join(bd,f)] 
 
 meifiles.sort()
 #meifiles = [ffile for ffile in files if os.path.splitext(ffile)[1] == '.mei']
@@ -148,12 +159,7 @@ textdb = couch['text'] # database for text
 # Iterate through each MEI file in directory
 from pymei.Import import convert
 nfile = 0
-for ffile in meifiles:
-    # nfile += 1
-    # if nfile == 3:
-    #     break
     print '\nProcessing ' + str(ffile) + '...'
-    #pagen = ffile.rsplit('_')[0]
     meifile = convert(str(ffile))
     page = meifile.search('page')
     pagen = int(page[0].attribute_by_name('n').value)
@@ -165,63 +171,63 @@ for ffile in meifiles:
     # get and store text
     lines = meifile.search('l')
     storeText(lines, zones, textdb)
-
+    
     #Set these to control which databases you access
     shortest_gram = 10
     longest_gram = 10
     for i in range(shortest_gram,longest_gram+1):
-       dbname = 'notegrams_'+str(i)
-       db = couch[dbname] #existing db
-          
-       # uncomment the lines below if you want to process only files that aren't already in the couch
-       # only proceed with the rest of the script if a query for pagen returns 0 hits
-       # map_fun = '''function(doc) {
-       #            emit(doc.pagen, null)
-       #        }'''
-       #rows = db.query(map_fun, key=pagen)
-       #lrows = len(rows)
-       lrows = 0 #comment out this line if you want to process files that aren't already in the couch
-       if lrows == 0:
-           #*******************TEST************************
-           # for note in notes:
-           #             s = meifile.get_system(note)
-           #             neume = str(note.parent.parent.attribute_by_name('name').value)
-           #             print 'pitch: '+ str(note.pitch[0])+ ' neume: ' + neume + " system: " +str(s)
-           #***********************************************
-       
-           print "Processing pitch sequences... "
-   
-           # for j,note in enumerate(notes):
-           for j in range(0,nnotes-i):
-               seq = notes[j:j+i]
-       
-               # get box coordinates of sequence
-               location = getLocation(seq, meifile, zones)
-               #print 'location: ' + str(location)
-       
-               # get neumes
-               neumes = getNeumes(seq)
+        dbname = 'notegrams_'+str(i)
+        db = couch[dbname] #existing db
+ 
+        # uncomment the lines below if you want to process only files that aren't already in the couch
+        # only proceed with the rest of the script if a query for pagen returns 0 hits
+        # map_fun = '''function(doc) {
+        #            emit(doc.pagen, null)
+        #        }'''
+        #rows = db.query(map_fun, key=pagen)
+        #lrows = len(rows)
+        lrows = 0 #comment out this line if you want to process files that aren't already in the couch
+        if lrows == 0:
+          #*******************TEST************************
+          # for note in notes:
+          #             s = meifile.get_system(note)
+          #             neume = str(note.parent.parent.attribute_by_name('name').value)
+          #             print 'pitch: '+ str(note.pitch[0])+ ' neume: ' + neume + " system: " +str(s)
+          #***********************************************
 
-               # get pitch names
-               [pnames, midipitch] = getPitchNames(seq)
-       
-               # get semitones
-               # calculate difference between each adjacent entry in midipitch list
-               semitones = [m-n for n, m in zip(midipitch[:-1], midipitch[1:])]
-               str_semitones = str(semitones)[1:-1] # string will be stored instead of array for easy searching
-       
-               # get quality invariant interval name and direction
-               # for example, an ascending major second and an ascending minor second will both be encoded as 'u2' 
-               # the only tritone to occur would be between b and f, in the context of this application we will assume that the be will always be sung as b flat
-               # thus the tritone is never encoded as such and will always be represented as either a fifth or a fourth, depending on inversion
-               intervals = getIntervals(semitones, pnames)
-           
-               # get contour - encode with Parsons code for musical contour
-               contour = getContour(semitones)
-       
-               # save new document
-               db.save({'pagen': int(pagen), 'pnames': pnames, 'neumes': neumes, 'contour': contour, 'semitones': str_semitones, 'intervals': intervals, 'location': location})
-           print db.info()
-       else:
-           print 'page ' + str(pagen) +  ' already processed\n'
+          print "Processing pitch sequences... "
+
+          # for j,note in enumerate(notes):
+          for j in range(0,nnotes-i):
+              seq = notes[j:j+i]
+
+              # get box coordinates of sequence
+              location = getLocation(seq, meifile, zones)
+              #print 'location: ' + str(location)
+
+              # get neumes
+              neumes = getNeumes(seq)
+
+              # get pitch names
+              [pnames, midipitch] = getPitchNames(seq)
+
+              # get semitones
+              # calculate difference between each adjacent entry in midipitch list
+              semitones = [m-n for n, m in zip(midipitch[:-1], midipitch[1:])]
+              str_semitones = str(semitones)[1:-1] # string will be stored instead of array for easy searching
+
+              # get quality invariant interval name and direction
+              # for example, an ascending major second and an ascending minor second will both be encoded as 'u2' 
+              # the only tritone to occur would be between b and f, in the context of this application we will assume that the be will always be sung as b flat
+              # thus the tritone is never encoded as such and will always be represented as either a fifth or a fourth, depending on inversion
+              intervals = getIntervals(semitones, pnames)
+  
+              # get contour - encode with Parsons code for musical contour
+              contour = getContour(semitones)
+
+              # save new document
+              db.save({'pagen': int(pagen), 'pnames': pnames, 'neumes': neumes, 'contour': contour, 'semitones': str_semitones, 'intervals': intervals, 'location': location})
+          print db.info()
+        else:
+          print 'page ' + str(pagen) +  ' already processed\n'
 
