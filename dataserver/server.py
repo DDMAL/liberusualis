8a644c3df5936ba2e90a418d2fc982a752786e83
Alastair Porter <alastair.porter@mail.mcgill.ca>
2011-12-01 12:34:02 -0500

    Handle a missing Accept: header

--- dataserver/server.py
+++ dataserver/server.py
@@ -23,7 +23,7 @@ class PageHandler(tornado.web.RequestHandler):
         page_number = pgno.rjust(4, "0")
         page_mei_file = os.path.join(conf.MEI_FILE_PATH, "{0}_corr.mei".format(page_number))
 
-        headers = self.request.headers.get("Accept").split(",")
+        headers = self.request.headers.get("Accept", "").split(",")
 
         print headers
 
